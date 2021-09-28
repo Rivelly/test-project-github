f20c5d7f796040831214d0ad9a5f6f0663f65470
Rivelly <r.ismanto@live.nl>
2021-09-28 11:51:28 +0200

    titel toegevoegd

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Hello!
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur itaque laudantium odit qui quod velit voluptatibus, voluptatum? Consequuntur culpa cupiditate deserunt distinctio dolor dolores eaque, ipsa nesciunt nulla officia, quo quod tenetur. Aliquam cupiditate natus, odit quibusdam quis quod tenetur?</p>
+      <li>fun</li>
     </div>
   );
 }
